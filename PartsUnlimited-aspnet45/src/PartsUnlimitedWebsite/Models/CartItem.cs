ea046e1ea0d3cd2d7bcf24831b851a4ff0810cc7
Mona Lisa <gunay_bm@yahoo.com>
2021-10-13 14:55:26 +0100

    commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //kkkkkkkk
+        //kkkkkkkkkkk
         [Key]
         public int CartItemId { get; set; }
 
